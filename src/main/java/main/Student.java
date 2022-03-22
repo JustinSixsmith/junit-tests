f910dd6d1ba3acc28229ac9f21003311bd33816f
Justin Sixsmith <justinsixsmith@gmail.com>
2022-03-22 09:32:35 -0500

    adds tests for getID and getName

--- src/main/java/main/Student.java
+++ src/main/java/main/Student.java
@@ -7,9 +7,19 @@ public class Student {
     private String name;
     private ArrayList<Integer> grades;
 
-    public Student(long id, String name, ArrayList<Integer> grades) {
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
-        this.grades = new ArrayList<>();
     }
 }
--- src/test/java/main/StudentTest.java
+++ src/test/java/main/StudentTest.java
@@ -2,12 +2,26 @@ package main;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    public static final long STUDENT_ID = 1;
+    public static String STUDENT_NAME = "Bob Smith";
 
     @Test
-    public void testIfIdIsLong() {
-
+    public void testGetID() {
+        Student s1 = new Student();
+        // Set the id
+        s1.setId(1);
+        // Test to make sure the id was set
+        assertEquals(STUDENT_ID, s1.getId());
     }
 
-
+    @Test
+    public void testGetName() {
+        Student s1 = new Student();
+        s1.setName(STUDENT_NAME);
+        assertEquals("Bob Smith", s1.getName());
+    }
 }
