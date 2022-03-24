418a99d509c7561dc14712540a24086aeaa636fd
Justin Sixsmith <justinsixsmith@gmail.com>
2022-03-24 13:51:05 -0500

    adds tests for Cohort class

--- src/test/java/main/CohortTest.java
+++ src/test/java/main/CohortTest.java
@@ -9,33 +9,48 @@ import java.util.Arrays;
 import static junit.framework.TestCase.assertEquals;
 
 public class CohortTest {
+
+    Cohort emptyCohort;
+    Cohort cohortWithOne;
+    Cohort cohortWithMany;
+
     public static final ArrayList<Integer> STUDENT_GRADES = new ArrayList<>(Arrays.asList(100, 90, 80));
 
     @Before
-    public void setup() {
-        Cohort cobol = new Cohort();
-        Student s1 = new Student(1, "Bob Dole");
-        for (Integer grade : STUDENT_GRADES) {
-            s1.addGrade(grade);
-        }
-        Student s2 = new Student(2, "Jack Palance");
-        for (Integer grade : STUDENT_GRADES) {
-            s2.addGrade(grade);
-        }
-        Student s3 = new Student(3, "Martha Stewart");
-        for (Integer grade : STUDENT_GRADES) {
-            s3.addGrade(grade);
-        }
-        Student s4 = new Student(3, "Jimi Hendrix");
-        for (Integer grade : STUDENT_GRADES) {
-            s4.addGrade(grade);
-        }
+    public void setUp() {
+        emptyCohort = new Cohort();
+        cohortWithOne = new Cohort();
+        cohortWithMany = new Cohort();
+
+        Student s1 = new Student(2, "Bob Newhart");
+        s1.addGrade(100);
+        s1.addGrade(90);
+        s1.addGrade(80);
+        Student s2 = new Student(2, "Jimi Hendrix");
+        s2.addGrade(100);
+        s2.addGrade(90);
+        s2.addGrade(80);
+        Student s3 = new Student(2, "Martha Stewart");
+        s3.addGrade(100);
+        s3.addGrade(90);
+        s3.addGrade(80);
+
+        cohortWithOne.addStudent(new Student(1, "Bob Smith"));
+
+        cohortWithMany.addStudent(s1);
+        cohortWithMany.addStudent(s2);
+        cohortWithMany.addStudent(s3);
     }
 
+    @Test
+    public void testAddStudent() {
+        assertEquals(1, cohortWithOne.getStudents().size());
+    }
 
-//    @Test
-//    private void testAddStudent() {
-//        assertEquals(false, );
-//    }
+    @Test
+    public void testCohortAverage() {
+        assertEquals(90, cohortWithMany.getCohortAverage(), 0);
+    }
+
 
 }
